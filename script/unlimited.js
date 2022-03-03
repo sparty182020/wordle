78fd93e014541a7f796d76109a45b596f2995f21
Benjamin <sparty182020@gmail.com>
2022-03-03 08:27:15 -0500

    Customization

--- script/unlimited.js
+++ script/unlimited.js
@@ -1,8 +1,11 @@
 class UCheat {
+    autoClose = false
     constructor(autoClose) {
-        this.autoClose = Boolean(autoClose)
+        if (autoClose != null) {
+            this.autoClose = Boolean(autoClose)
+        }
     }
-    solve() {
+    static solve() {
         if (location.host !== "www.wordleunlimited.com") { return }
         const x = document.getElementsByClassName("game-id")[0].innerText.replace("Game ID: ", "")
         const solution = atob(x)
@@ -15,16 +18,67 @@ class UCheat {
             document.dispatchEvent(new KeyboardEvent('keydown', { 'key': `${spSolution[i]}` }))
         }
         if (this.autoClose == true) {
-            Promise.resolve()
-                .then(
-                    setTimeout(document.getElementsByClassName("btn-close")[0].click(), 500)
-                )
-                .then(
-                    setTimeout(document.dispatchEvent(new KeyboardEvent('keydown', {key = 'Enter'})))
-                )
+            setTimeout(_ => {
+                Promise.resolve()
+                    .then(
+                        setTimeout(document.getElementsByClassName("btn-close")[0].click(), 500)
+                    )
+                    .then(
+                        setTimeout(document.dispatchEvent(new KeyboardEvent('keydown', { 'key': 'Enter' })))
+                    )
+            },
+                500)
         }
     }
-    autoSolve() {
+    static autoSolve() {
+        if (location.host !== "www.wordleunlimited.com") { return }
+        const x = document.getElementsByClassName("game-id")[0].innerText.replace("Game ID: ", "")
+        const solution = atob(x)
+        const spSolution = solution.split('')
+        for (let i = 0; i <= solution.length; i++) {
+            if (i == solution.length) {
+                document.dispatchEvent(new KeyboardEvent('keydown', { 'key': 'Enter' }))
+                break
+            }
+            document.dispatchEvent(new KeyboardEvent('keydown', { 'key': `${spSolution[i]}` }))
+        }
+        Promise.resolve()
+            .then(
+                setTimeout(_ => { document.getElementsByClassName("btn-close")[0].click() }, 500)
+            )
+            .then(
+                setTimeout(_ => { document.dispatchEvent(new KeyboardEvent('keydown', { 'key': 'Enter' })) }, 1000)
+            )
+            .then(
+                setTimeout(_ => { UCheat.autoSolve() }, 1000)
+            )
+    }
+    cSolve() {
+        if (location.host !== "www.wordleunlimited.com") { return }
+        const x = document.getElementsByClassName("game-id")[0].innerText.replace("Game ID: ", "")
+        const solution = atob(x)
+        const spSolution = solution.split('')
+        for (let i = 0; i <= solution.length; i++) {
+            if (i == solution.length) {
+                document.dispatchEvent(new KeyboardEvent('keydown', { 'key': 'Enter' }))
+                break
+            }
+            document.dispatchEvent(new KeyboardEvent('keydown', { 'key': `${spSolution[i]}` }))
+        }
+        if (this.autoClose == true) {
+            setTimeout(_ => {
+                Promise.resolve()
+                    .then(
+                        setTimeout(document.getElementsByClassName("btn-close")[0].click(), 500)
+                    )
+                    .then(
+                        setTimeout(document.dispatchEvent(new KeyboardEvent('keydown', { 'key': 'Enter' })))
+                    )
+            },
+                500)
+        }
+    }
+    cAutoSolve() {
         if (location.host !== "www.wordleunlimited.com") { return }
         const x = document.getElementsByClassName("game-id")[0].innerText.replace("Game ID: ", "")
         const solution = atob(x)
