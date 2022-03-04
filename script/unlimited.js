72581401896b2b12cd571b1ab5a88dbacad3ec02
Benjamin <sparty182020@gmail.com>
2022-03-03 21:44:17 -0500

    button

--- script/unlimited.js
+++ script/unlimited.js
@@ -40,18 +40,17 @@ class UCheat {
                 document.dispatchEvent(new KeyboardEvent('keydown', { 'key': 'Enter' }))
                 break
             }
-            document.dispatchEvent(new KeyboardEvent('keydown', { 'key': `${spSolution[i]}` }))
+            Promise.resolve()
+                .then(
+                    setTimeout(_ => { document.getElementsByClassName("btn-close")[0].click() }, 500)
+                )
+                .then(
+                    setTimeout(_ => { document.dispatchEvent(new KeyboardEvent('keydown', { 'key': 'Enter' })) }, 1000)
+                )
+                .then(
+                    setTimeout(_ => { this.autoSolve() }, 1000)
+                )
         }
-        Promise.resolve()
-            .then(
-                setTimeout(_ => { document.getElementsByClassName("btn-close")[0].click() }, 500)
-            )
-            .then(
-                setTimeout(_ => { document.dispatchEvent(new KeyboardEvent('keydown', { 'key': 'Enter' })) }, 1000)
-            )
-            .then(
-                setTimeout(_ => { UCheat.autoSolve() }, 1000)
-            )
     }
     cSolve() {
         if (location.host !== "www.wordleunlimited.com") { return }
@@ -101,4 +100,15 @@ class UCheat {
                 setTimeout(_ => { UCheat.autoSolve() }, 1000)
             )
     }
+}
+
+const genButton = function () {
+    const buttondiv = document.createElement("div")
+    buttondiv.style.cssText = "left: 5vw; top: 50vh; width: 128px; position: absolute;"
+    const button = document.createElement("button")
+    button.onclick = _ => new UCheat(true).cSolve()
+    button.innerText = "Solve this Wordle"
+    button.style.cssText = "border: 4px solid black; border-radius: 16px; position: relative; background: linear-gradient(45deg, red, blue);"
+    buttondiv.appendChild(button)
+    document.body.appendChild(buttondiv)
 }
